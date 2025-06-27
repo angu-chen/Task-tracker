436d176a81f302d115e1186f68943360f4993c27
Angu Chen <anguchen96@gmail.com>
2025-06-27 12:16:26 +1200

    initial commit

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,7 +3,9 @@ import { Outlet } from 'react-router'
 function App() {
   return (
     <div>
-      <h1>App Header</h1>
+      <div className="block m-auto px-10 py-5">
+        <h1 className="text-7xl text-center">Task Manager</h1>
+      </div>
       <Outlet />
     </div>
   )
--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="flex flex-col items-center mt-10">
+      <h1 className="text-4xl">Home</h1>
       <p>React development has begun!</p>
     </div>
   )
